66be8a637713de405e6c09dd9407470f7d6b05ca
Vladimir <vladimir@vladimir.accedo.dk>
2013-07-26 16:46:56 +0300

    Initial commit

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,17 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Vladimir on 7/26/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+//+(NSString *)classFromCharacterType:(CharacterClassType)type;
+//+(NSString *)raceFromRaceType:(CharacterRaceType)type;
+//+(NSString *)qualityFromQualityType:(ItemQuality)quality;
+
+@end
